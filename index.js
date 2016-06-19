d8283b9015f7a80fb4670c2790ad2f72476ba673
Dmitriy Zhuk <dmitriyzhuk@gmail.com>
2016-06-19 21:50:54 +0300

    config

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 version = '0.0.0';
 var	express = require('express');
+	global = require('./config');
 
 	fs = require('fs');
 
 	//config
 	config = require('nconf');
-	config.add("config", {type: 'file', file:'config.json'});
-
 	app = express();
 
 	//router
 	router = express.Router();
 	app.use(router);
-	controller = require(config.get('dir').system+'engine/controller');
+	controller = require(global.dir.system+'engine/controller');
 	controller.init(app);
 
 app.listen(3000,function(){
--- system/engine/controller.js
+++ system/engine/controller.js
@@ -1,22 +1,19 @@
 module.exports = {
 	init : function(app) {
-		fs.readdirSync(config.get('dir').app+'controller').forEach(function (folder) {
+		fs.readdirSync(global.dir.app+'controller').forEach(function (folder) {
 
-			if(fs.lstatSync(config.get('dir').app+'controller/'+folder).isDirectory()){
+			if(fs.lstatSync(global.dir.app+'controller/'+folder).isDirectory()){
 
-				fs.readdirSync(config.get('dir').app+'controller/'+folder).forEach(function (file) {
+				fs.readdirSync(global.dir.app+'controller/'+folder).forEach(function (file) {
 
 					if(file.substr(-3) == '.js') {
 
-						controller = require(config.get('dir').app + 'controller/' + folder + '/' + file);
+						controller = require(global.dir.app + 'controller/' + folder + '/' + file);
 						controller.controller(app);
 
 					}
-
 				});
-
 			}
-
 		});
 	}
 }
